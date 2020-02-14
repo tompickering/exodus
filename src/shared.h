6e4b3cdacddedf731900cd65de37b3b1dc70d84f
Tom Pickering <tom@alephspace.net>
2020-02-14 04:51:09 +0000

    Shared manager references

--- src/shared.h
+++ src/shared.h
@@ -1,16 +1,28 @@
 #ifndef GUARD_SHARED_H
 #define GUARD_SHARED_H
 
+typedef struct {
+    int x;
+    int y;
+} MousePos;
+
 #include "logger.h"
 #include "platform.h"
+
+#include "draw/draw.h"
+#include "audio/audio.h"
 
 #define PROG_NAME "Exodus"
 
 extern Log::Logger L;
 
-typedef struct {
-    int x;
-    int y;
-} MousePos;
+// We need forward-declarations here so manager
+// headers which #include this file don't see
+// an as-yet undefined type.
+class DRAWMANAGER;
+class AUDIOMANAGER;
+
+extern DRAWMANAGER draw_manager;
+extern AUDIOMANAGER audio_manager;
 
 #endif
