da94544ec1c669f3f3e990a33208343f7e1e8a21
Tom Pickering <tom@alephspace.net>
2020-02-15 06:56:47 +0000

    SDL init

--- src/exodus.SDL.cpp
+++ src/exodus.SDL.cpp
@@ -2,6 +2,9 @@
 
 #include "shared.h"
 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
 ExodusSDL::ExodusSDL() {
 }
 
@@ -12,6 +15,20 @@ bool ExodusSDL::init() {
     bool res = Exodus::init();
     if (!res)
         return res;
+
     L.info("Initialising SDL...");
+
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
+        L.fatal("Could not initialise SDL");
+        return false;
+    }
+
+    if (TTF_Init() < 0) {
+        L.fatal("Could not initialise SDL_ttf");
+        return false;
+    }
+
+    L.info("SDL initialised.");
+
     return res;
 }
