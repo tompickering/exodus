57ca937f0ff6f6eb2449b47a9aa4e0ce5f0bc0ec
Tom Pickering <tom@alephspace.net>
2020-03-07 18:44:36 +0000

    Add intro img assets

--- src/assetpaths.cpp
+++ src/assetpaths.cpp
@@ -2,16 +2,118 @@
 
 const char *FONT_AUDIOWIDE = "font/audiowide/Audiowide-Regular.ttf";
 
-#define D_INTRO "assets/img/scene/intro/"
-const char *IMG_INTRO_EARTH = D_INTRO "earth";
-const char *IMG_INTRO_CITY  = D_INTRO "city";
+#define D_INTRO_SCENE "assets/img/scene/intro/"
+const char *IMG_INTRO_EARTH = D_INTRO_SCENE "earth";
+const char *IMG_INTRO_CITY  = D_INTRO_SCENE "city";
+
+#define D_INTRO "assets/img/intro/"
+const char *IMG_INTRO_BT1_FALL1    = D_INTRO "BT1_fall1";
+const char *IMG_INTRO_BT1_FALL2    = D_INTRO "BT1_fall2";
+const char *IMG_INTRO_BT1_FALL3    = D_INTRO "BT1_fall3";
+const char *IMG_INTRO_BT1_FALL4    = D_INTRO "BT1_fall4";
+const char *IMG_INTRO_BT1_FALL5    = D_INTRO "BT1_fall5";
+const char *IMG_INTRO_BT1_FALL6    = D_INTRO "BT1_fall6";
+const char *IMG_INTRO_BT1_FALL7    = D_INTRO "BT1_fall7";
+const char *IMG_INTRO_FI1_FIRE1    = D_INTRO "FI1_fire1";
+const char *IMG_INTRO_FI1_FIRE2    = D_INTRO "FI1_fire2";
+const char *IMG_INTRO_FI1_FIRE3    = D_INTRO "FI1_fire3";
+const char *IMG_INTRO_FI1_FIRE4    = D_INTRO "FI1_fire4";
+const char *IMG_INTRO_FI1_FIRE5    = D_INTRO "FI1_fire5";
+const char *IMG_INTRO_FI1_FIRE6    = D_INTRO "FI1_fire6";
+const char *IMG_INTRO_FI1_SHOT1    = D_INTRO "FI1_shot1";
+const char *IMG_INTRO_FI1_SHOT2    = D_INTRO "FI1_shot2";
+const char *IMG_INTRO_FI1_SHOT3    = D_INTRO "FI1_shot3";
+const char *IMG_INTRO_FI1_SHOT4    = D_INTRO "FI1_shot4";
+const char *IMG_INTRO_FI1_SHOT5    = D_INTRO "FI1_shot5";
+const char *IMG_INTRO_GU1_GUARD    = D_INTRO "GU1_guard";
+const char *IMG_INTRO_OD1_BEHIND   = D_INTRO "OD1_behind";
+const char *IMG_INTRO_OD1_DOOR_L   = D_INTRO "OD1_door_l";
+const char *IMG_INTRO_OD1_DOOR_R   = D_INTRO "OD1_door_r";
+const char *IMG_INTRO_OD1_FRAME    = D_INTRO "OD1_frame";
+const char *IMG_INTRO_OD1_OFF      = D_INTRO "OD1_off";
+const char *IMG_INTRO_OD1_ON       = D_INTRO "OD1_on";
+const char *IMG_INTRO_OD1_SEPP     = D_INTRO "OD1_sepp";
+const char *IMG_INTRO_PH1_PUSH_1   = D_INTRO "PH1_push_1";
+const char *IMG_INTRO_PH1_PUSH_2   = D_INTRO "PH1_push_2";
+const char *IMG_INTRO_PH1_PUSH_3   = D_INTRO "PH1_push_3";
+const char *IMG_INTRO_PH1_PUSH_4   = D_INTRO "PH1_push_4";
+const char *IMG_INTRO_PH1_PUSH_5   = D_INTRO "PH1_push_5";
+const char *IMG_INTRO_PH1_PUSH_6   = D_INTRO "PH1_push_6";
+const char *IMG_INTRO_PH1_PUSH_7   = D_INTRO "PH1_push_7";
+const char *IMG_INTRO_PH1_PUSH_8   = D_INTRO "PH1_push_8";
+const char *IMG_INTRO_PH1_PUSH_9   = D_INTRO "PH1_push_9";
+const char *IMG_INTRO_SH1_SHIP     = D_INTRO "SH1_ship";
+const char *IMG_INTRO_SH1_SHIP2    = D_INTRO "SH1_ship2";
+const char *IMG_INTRO_SH2_SHIP     = D_INTRO "SH2_ship";
+const char *IMG_INTRO_SH3_CRUISER  = D_INTRO "SH3_cruiser";
+const char *IMG_INTRO_SH3_CRUISER1 = D_INTRO "SH3_cruiser1";
+const char *IMG_INTRO_SH3_CRUISER2 = D_INTRO "SH3_cruiser2";
+const char *IMG_INTRO_SH3_SHUTTLE  = D_INTRO "SH3_shuttle";
+const char *IMG_INTRO_SH4_BLINK1   = D_INTRO "SH4_blink1";
+const char *IMG_INTRO_SH4_BLINK2   = D_INTRO "SH4_blink2";
+const char *IMG_INTRO_SH4_BLINK3   = D_INTRO "SH4_blink3";
+const char *IMG_INTRO_SH4_MOVEOUT  = D_INTRO "SH4_moveout";
+const char *IMG_INTRO_SH4_SHUTTLE1 = D_INTRO "SH4_shuttle1";
+const char *IMG_INTRO_SH4_SHUTTLE2 = D_INTRO "SH4_shuttle2";
+const char *IMG_INTRO_SH4_SHUTTLE3 = D_INTRO "SH4_shuttle3";
+const char *IMG_INTRO_SH4_SHUTTLE4 = D_INTRO "SH4_shuttle4";
 
 #endif  // LINUX
 
 const char *ASSETS_IMG[] = {
     IMG_INTRO_EARTH,
     IMG_INTRO_CITY,
+
+    IMG_INTRO_BT1_FALL1,
+    IMG_INTRO_BT1_FALL2,
+    IMG_INTRO_BT1_FALL3,
+    IMG_INTRO_BT1_FALL4,
+    IMG_INTRO_BT1_FALL5,
+    IMG_INTRO_BT1_FALL6,
+    IMG_INTRO_BT1_FALL7,
+    IMG_INTRO_FI1_FIRE1,
+    IMG_INTRO_FI1_FIRE2,
+    IMG_INTRO_FI1_FIRE3,
+    IMG_INTRO_FI1_FIRE4,
+    IMG_INTRO_FI1_FIRE5,
+    IMG_INTRO_FI1_FIRE6,
+    IMG_INTRO_FI1_SHOT1,
+    IMG_INTRO_FI1_SHOT2,
+    IMG_INTRO_FI1_SHOT3,
+    IMG_INTRO_FI1_SHOT4,
+    IMG_INTRO_FI1_SHOT5,
+    IMG_INTRO_GU1_GUARD,
+    IMG_INTRO_OD1_BEHIND,
+    IMG_INTRO_OD1_DOOR_L,
+    IMG_INTRO_OD1_DOOR_R,
+    IMG_INTRO_OD1_FRAME,
+    IMG_INTRO_OD1_OFF,
+    IMG_INTRO_OD1_ON,
+    IMG_INTRO_OD1_SEPP,
+    IMG_INTRO_PH1_PUSH_1,
+    IMG_INTRO_PH1_PUSH_2,
+    IMG_INTRO_PH1_PUSH_3,
+    IMG_INTRO_PH1_PUSH_4,
+    IMG_INTRO_PH1_PUSH_5,
+    IMG_INTRO_PH1_PUSH_6,
+    IMG_INTRO_PH1_PUSH_7,
+    IMG_INTRO_PH1_PUSH_8,
+    IMG_INTRO_PH1_PUSH_9,
+    IMG_INTRO_SH1_SHIP,
+    IMG_INTRO_SH1_SHIP2,
+    IMG_INTRO_SH2_SHIP,
+    IMG_INTRO_SH3_CRUISER,
+    IMG_INTRO_SH3_CRUISER1,
+    IMG_INTRO_SH3_CRUISER2,
+    IMG_INTRO_SH3_SHUTTLE,
+    IMG_INTRO_SH4_BLINK1,
+    IMG_INTRO_SH4_BLINK2,
+    IMG_INTRO_SH4_BLINK3,
+    IMG_INTRO_SH4_MOVEOUT,
+    IMG_INTRO_SH4_SHUTTLE1,
+    IMG_INTRO_SH4_SHUTTLE2,
+    IMG_INTRO_SH4_SHUTTLE3,
+    IMG_INTRO_SH4_SHUTTLE4,
+
     "",
 };
-
-
