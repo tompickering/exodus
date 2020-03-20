00f557156a8ddcabfa26f6e48236c9dfd38ccd8b
Tom Pickering <tom@alephspace.net>
2020-03-20 21:47:28 +0000

    Menu size options

--- src/mode/menu.cpp
+++ src/mode/menu.cpp
@@ -2,9 +2,16 @@
 
 #include "../assetpaths.h"
 
+#define OPT_0 {RES_X - 95, 147, 1.0, 0.5, 1, 1}
+#define OPT_1 {95, 280, 0.0, 0.5, 1, 1}
+#define OPT_2 {RES_X - 95, 413, 1.0, 0.5, 1, 1}
+
 enum ID {
     NEWGAME_TXT,
     LOADGAME_TXT,
+    GAL_SZ_SMALL,
+    GAL_SZ_MEDIUM,
+    GAL_SZ_LARGE,
     END,
 };
 
@@ -13,32 +20,111 @@ Menu::Menu() : ModeBase("Menu") {
 
 void Menu::enter() {
     ModeBase::enter(ID::END);
+    stage = Main;
     draw_manager.pixelswap_draw(IMG_BG_STARS2);
     draw_manager.pixelswap_draw_text(Font::Large, "Please select.", Justify::Centre, RES_X/2, 135, {0xEE, 0xEE, 0xAA});
     draw_manager.pixelswap_draw_text(id(NEWGAME_TXT), Font::Large, "Conquer the stars", Justify::Centre, RES_X/2, 205, {0xFF, 0xFF, 0xFF});
     draw_manager.pixelswap_draw_text(id(LOADGAME_TXT), Font::Large, "Load a saved game", Justify::Centre, RES_X/2, 250, {0xFF, 0xFF, 0xFF});
     draw_manager.pixelswap_start();
-    pixelswap_done = false;
+    trans_state = Started;
 }
 
+/*
+ * Main > size > #humans > name/geneder/welcome > flag > aim > other races > confirm
+ */
 ExodusMode Menu::update(float delta) {
-    if (draw_manager.pixelswap_active()) {
+    if (draw_manager.pixelswap_active() || draw_manager.fade_active()) {
+        draw_manager.show_cursor(false);
         return ExodusMode::MODE_None;
     }
 
-    if (!pixelswap_done) {
-        pixelswap_done = true;
-        draw_manager.save_background();
-        draw_manager.show_cursor(true);
-    }
+    switch(stage) {
+        case Main:
+            if (trans_state == Started) {
+                trans_state = Done;
+                draw_manager.save_background();
+                draw_manager.show_cursor(true);
+            }
 
-    if (draw_manager.query_click(id(NEWGAME_TXT)).id) {
-        L.debug("New game");
-    }
+            if (draw_manager.query_click(id(NEWGAME_TXT)).id) {
+                set_stage(Size);
+                draw_manager.fade_black(1.2f, 24);
+            }
 
-    if (draw_manager.query_click(id(LOADGAME_TXT)).id) {
-        L.debug("Load game");
+            if (draw_manager.query_click(id(LOADGAME_TXT)).id) {
+                L.debug("Load game");
+            }
+            break;
+        case Load:
+            break;
+        case Size:
+            if (trans_state == None) {
+                draw_manager.pixelswap_draw(IMG_BG_MENU0);
+                draw_manager.pixelswap_draw_text(Font::Large, "Galaxy size", Justify::Left, 20, 30, {0xEE, 0xEE, 0xAA});
+                draw_manager.pixelswap_draw(
+                        id(GAL_SZ_SMALL),
+                        IMG_STARTGR_GAL_S,
+                        OPT_0);
+                draw_manager.pixelswap_draw(
+                        id(GAL_SZ_MEDIUM),
+                        IMG_STARTGR_GAL_M,
+                        OPT_1);
+                draw_manager.pixelswap_draw(
+                        id(GAL_SZ_MEDIUM),
+                        IMG_STARTGR_GAL_L,
+                        OPT_2);
+                draw_manager.pixelswap_start();
+                trans_state = Started;
+            } else if (trans_state == Started) {
+                trans_state = Done;
+                draw_manager.save_background();
+                draw_manager.show_cursor(true);
+            } else {
+                bool progress = false;
+                if (draw_manager.query_click(id(GAL_SZ_SMALL)).id) {
+                    config.size = GAL_Small;
+                    progress = true;
+                } else if (draw_manager.query_click(id(GAL_SZ_MEDIUM)).id) {
+                    config.size = GAL_Medium;
+                    progress = true;
+                } else if (draw_manager.query_click(id(GAL_SZ_LARGE)).id) {
+                    config.size = GAL_Large;
+                    progress = true;
+                }
+
+                if (progress)
+                    set_stage(NPlayers);
+            }
+
+            break;
+        case NPlayers:
+            if (trans_state == None) {
+                draw_manager.draw(IMG_BG_MENU0);
+                draw_manager.draw_text(
+                        "How many human players wish to play?",
+                        Justify::Centre, RES_X/2, 90, {0xEE, 0xEE, 0xAA});
+                trans_state = Done;
+            }
+
+            break;
+        case Name:
+            break;
+        case Title:
+            break;
+        case Flag:
+            break;
+        case Aim:
+            break;
+        case Difficulty:
+            break;
+        case Confirm:
+            break;
     }
 
     return ExodusMode::MODE_None;
+}
+
+void Menu::set_stage(Stage new_stage) {
+    trans_state = None;
+    stage = new_stage;
 }
--- src/mode/menu.h
+++ src/mode/menu.h
@@ -6,13 +6,69 @@
 #include "../platform.h"
 #include "../draw/draw.h"
 
+enum GalaxySize {
+    GAL_Small,
+    GAL_Medium,
+    GAL_Large,
+};
+
+typedef struct {
+    char name[16];
+} PlayerInfo;
+
+enum Aim {
+    AIM_Might,
+    AIM_Money,
+    AIM_Civilization,
+};
+
+enum EnemyStart {
+    ENEMY_None,
+    ENEMY_Weak,
+    ENEMY_Medium,
+    ENEMY_Strong,
+};
+
+typedef struct {
+    GalaxySize size;
+    unsigned char n_players;
+    PlayerInfo info[4];
+    Aim aim;
+    EnemyStart enemy_start;
+} GameConfig;
+
 class Menu : ModeBase {
     public:
         Menu();
         virtual void enter() override;
         virtual ExodusMode update(float) override;
     private:
-        bool pixelswap_done;
+        enum Stage {
+            Main,
+            Load,
+            Size,
+            NPlayers,
+            Name,
+            Title,
+            Flag,
+            Aim,
+            Difficulty,
+            Confirm,
+        };
+
+        enum TransitionState {
+            None,
+            Started,
+            Done,
+        };
+
+        Stage stage;
+        TransitionState trans_state;
+
+        void set_stage(Stage);
+
+        // The purpose of the Menu is to init this game config
+        GameConfig config;
 };
 
 #endif
