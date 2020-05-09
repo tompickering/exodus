90a9bf67fc1b942d9f63fd1c526cc587ef3a797c
Tom Pickering <tom@alephspace.net>
2020-05-09 20:13:16 +0100

    Ownership info

--- src/mode/planet_status.cpp
+++ src/mode/planet_status.cpp
@@ -28,19 +28,26 @@ void PlanetStatus::enter() {
 
     char heading[12 + PLANET_MAX_NAME];
 
-    char x_pad = 30;
+    char pad_x = 30;
     PlayerInfo *owner = p->is_owned() ? exostate.get_player(p->get_owner()) : nullptr;
+
+    char ownership[PLANET_MAX_NAME + MAX_PLAYER_FULLNAME + 31];
 
     if (owner) {
         snprintf(heading, 12 + PLANET_MAX_NAME, "The planet %s", p->get_name());
-        x_pad += 70;
+        pad_x += 86;
 
         draw_manager.draw(
             id(ID::FLAG),
             flags[owner->flag_idx],
             {12, 100, 0, 0, 1, 1});
+
+        snprintf(ownership, PLANET_MAX_NAME + MAX_PLAYER_FULLNAME + 30,
+                 "%s belongs to %s.", p->get_name(), owner->full_name);
     } else {
         strncpy(heading, "Unexplored planet", 12 + PLANET_MAX_NAME);
+        strncpy(ownership, "The planet belongs to no confederation.",
+                PLANET_MAX_NAME + MAX_PLAYER_FULLNAME + 30);
     }
 
     draw_manager.draw_text(
@@ -49,6 +56,13 @@ void PlanetStatus::enter() {
         Justify::Centre,
         RES_X/2, 16,
         COL_TEXT);
+
+    draw_manager.draw_text(
+        ownership,
+        Justify::Left,
+        pad_x, 90,
+        COL_TEXT2);
+
 
     draw_manager.save_background();
     draw_manager.show_cursor(true);
