8bd797d01a766b520a087d7184c1497d641f485c
Tom Pickering <tom@alephspace.net>
2020-05-25 21:45:35 +0100

    Seed RNG with seconds since epoch

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include "platform.h"
 #include "exodus.h"
 
+#include <ctime>
+#include <cstdlib>
+
 EXODUS exodus;
 
 int main(int argc, char** argv) {
+    srand((unsigned int)time(nullptr));
     return exodus.run(argc, argv);
 }
