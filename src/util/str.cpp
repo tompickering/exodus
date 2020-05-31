951308dd74e674453cf93028ea090f7a36abd4ad
Tom Pickering <tom@alephspace.net>
2020-05-31 17:25:56 +0100

    Capitalisation util

---
+++ src/util/str.cpp
@@ -0,0 +1,18 @@
+#include "str.h"
+
+#include <cstring>
+#include <cctype>
+
+#define CAP_MAX_LEN 512
+
+char _tmp_caps[CAP_MAX_LEN];
+
+const char* tmp_caps(const char* str) {
+    size_t i;
+    for (i = 0; i < strnlen(str, CAP_MAX_LEN - 1); ++i) {
+        if (str[i] == '\0') break;
+        _tmp_caps[i] = toupper(str[i]);
+    }
+    _tmp_caps[i] = '\0';
+    return (const char*)(&_tmp_caps);
+}
---
+++ src/util/str.h
@@ -0,0 +1,6 @@
+#ifndef GUARD_UTIL_STR_H
+#define GUARD_UTIL_STR_H
+
+const char* tmp_caps(const char*);
+
+#endif
