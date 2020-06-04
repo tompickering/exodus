1854666db8a5b705158bee55df6ce61d5130b62b
Tom Pickering <tom@alephspace.net>
2020-06-04 21:13:17 +0100

    Fix segfault

--- src/galaxy/planet.cpp
+++ src/galaxy/planet.cpp
@@ -523,7 +523,7 @@ void Planet::discard_excess_resources() {
     int total = 0;
     for (int i = 0; i < 8; ++i) total += d[i];
     if (total > 0) {
-        L.info("%s: DISCARDED %d/%d/%d/%d/%d/%d/%d/%d",
+        L.info("%s: DISCARDED %d/%d/%d/%d/%d/%d/%d/%d", get_name(),
                d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7]);
     }
 }
