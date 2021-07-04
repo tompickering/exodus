71216e3fcf5cff8b8ac5d7745abe90c258ddb038
Tom Pickering <tom@alephspace.net>
2021-07-04 19:25:55 +0100

    Space battle framework

--- src/mode/space_battle.cpp
+++ src/mode/space_battle.cpp
@@ -26,10 +26,46 @@ void SpaceBattle::enter() {
     L.debug("STARTING BATTLE - ENEMY SHIPS: %d", b.enemy_ships);
 }
 
+void SpaceBattle::prepare() {
+    Player *p = exostate.get_active_player();
+    SpaceBattleParams &b = ephstate.space_battle;
+
+    selected = -1;
+
+    for (int i = 0; i < MAX_SHIPS; ++i) {
+        ships[i].exists = false;
+    }
+
+    int shields = p->get_starship().shield_generators;
+    ships[0].init(SHIP_Starship, false, 640, 512, 1, shields*5);
+}
+
+void SpaceBattle::draw() {
+    for (int i = 0; i < MAX_SHIPS; ++i) {
+        if (!ships[i].exists) {
+            continue;
+        }
+
+        const BattleShip& s = ships[i];
+
+        draw_manager.draw(
+            s.spr_id,
+            selected == i ? s.spr_sel : s.spr,
+            {s.x/2, s.y/2,
+             .5f, .5f, 1, 1});
+    }
+}
+
 ExodusMode SpaceBattle::update(float delta) {
 
     switch (stage) {
         case SB_Setup:
+            {
+                prepare();
+                stage = SB_Battle;
+            }
+            break;
+        case SB_Battle:
             {
                 if (draw_manager.clicked()) {
                     stage = SB_Exit;
@@ -38,11 +74,55 @@ ExodusMode SpaceBattle::update(float delta) {
             break;
         case SB_Exit:
             {
+                for (int i = 0; i < MAX_SHIPS; ++i) {
+                    ships[i].cleanup();
+                }
+
                 ephstate.set_ephemeral_state(EPH_ResumeFly);
                 return ExodusMode::MODE_Pop;
             }
             break;
     }
 
+    draw();
+
     return ExodusMode::MODE_None;
+}
+
+void BattleShip::init(BattleShipType _type, bool _enemy, int _x, int _y, int _hp) {
+    init(_type, _enemy, _x, _y, _hp, 0);
+}
+
+void BattleShip::init(BattleShipType _type, bool _enemy, int _x, int _y, int _hp, int _shield) {
+    exists = true;
+    type = _type;
+    enemy = _enemy;
+    x = _x;
+    y = _y;
+    hp = _hp;
+    shield_max = _shield;
+    shield = shield_max;
+    target = -1;
+
+    spr_id = draw_manager.new_sprite_id();
+
+    spr = IMG_RD1_SYMBOL3;
+    spr_sel = IMG_RD1_SYMBOL3S;
+
+    if (!enemy) {
+        if (type == SHIP_Starship) {
+            spr = IMG_RD1_SYMBOL2;
+            spr_sel = IMG_RD1_SYMBOL2S;
+        } else {
+            spr = IMG_RD1_SYMBOL1;
+            spr_sel = IMG_RD1_SYMBOL1S;
+        }
+    }
+}
+
+void BattleShip::cleanup() {
+    if (exists) {
+        draw_manager.release_sprite_id(spr_id);
+    }
+    exists = false;
 }
--- src/mode/space_battle.h
+++ src/mode/space_battle.h
@@ -7,8 +7,48 @@
 
 #include "state/exodus_state.h"
 
+#include "assetpaths.h"
+
+#define MAX_SHIPS_PER_TYPE 5
+#define MAX_SHIPS (MAX_SHIPS_PER_TYPE * 4)
+
 extern ExodusState exostate;
 extern EphemeralState ephstate;
+extern DRAWMANAGER draw_manager;
+
+enum BattleShipType {
+    SHIP_Starship,
+    SHIP_Warship,
+    SHIP_Bomber,
+    SHIP_Scout,
+};
+
+enum BattleShipAction {
+    BSA_Idle,
+    BSA_Attack,
+    BSA_Report,
+};
+
+struct BattleShip {
+    void init(BattleShipType, bool, int, int, int);
+    void init(BattleShipType, bool, int, int, int, int);
+    void cleanup();
+
+    BattleShipType type;
+    bool exists;
+    bool enemy;
+    int x;
+    int y;
+    int hp;
+    int shield;
+    int shield_max;
+    BattleShipAction action;
+    int target;
+    const char *spr;
+    const char *spr_sel;
+
+    SprID spr_id;
+};
 
 class SpaceBattle : ModeBase {
     public:
@@ -16,12 +56,19 @@ class SpaceBattle : ModeBase {
         virtual void enter() override;
         virtual ExodusMode update(float) override;
     private:
+        void prepare();
+        void draw();
+
         enum Stage {
             SB_Setup,
+            SB_Battle,
             SB_Exit,
         };
 
         Stage stage;
+
+        BattleShip ships[MAX_SHIPS];
+        int selected;
 };
 
 #endif
