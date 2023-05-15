92892aaa3bfbf80d66f554f6472f065344bf8371
Tom Pickering <tom@alephspace.net>
2023-05-15 23:32:33 +0100

    Simplification

--- src/state/exodus_state.cpp
+++ src/state/exodus_state.cpp
@@ -851,19 +851,8 @@ void ExodusState::unset_alliances(int a, int b) {
 
 int ExodusState::get_total_net_income(int player_idx) {
     int total = 0;
-    Galaxy *gal = get_galaxy();
-    int n_stars;
-    Star *stars = gal->get_stars(n_stars);
-    for (int star_idx = 0; star_idx < n_stars; ++star_idx) {
-        Star *s = &stars[star_idx];
-        for (int planet_idx = 0; planet_idx < STAR_MAX_PLANETS; ++planet_idx) {
-            Planet *p = s->get_planet(planet_idx);
-            if (p && p->exists() && p->is_owned()) {
-                if (p->get_owner() == player_idx) {
-                    total += p->get_net_income();
-                }
-            }
-        }
+    for (PlanetIterator piter(player_idx); !piter.complete(); ++piter) {
+        total += piter->get_net_income();
     }
     return total;
 }
