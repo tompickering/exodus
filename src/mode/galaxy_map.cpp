218c746e193668be44072c0b5ab1e0f1235868cc
Tom Pickering <tom@alephspace.net>
2023-05-27 10:52:40 +0100

    Music track correction

--- src/mode/galaxy_map.cpp
+++ src/mode/galaxy_map.cpp
@@ -2070,7 +2070,7 @@ ExodusMode GalaxyMap::month_pass_update() {
                         p->set_guild_title(new_title);
                         const char* name = p->get_full_name();
                         const char* title_str = p->get_guild_title_str();
-                        audio_manager.target_music(mpart2mus(10));
+                        audio_manager.target_music(mpart2mus(5));
                         bulletin_start_new(false);
                         bulletin_set_flag(IMG_TS1_FLAG16);
                         bulletin_set_text_col(COL_TEXT3);
