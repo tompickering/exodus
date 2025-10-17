b523b4b1227de500c29a44892b20a9bfde32d7f0
Tom Pickering <tom@alephspace.net>
2025-10-17 23:25:06 +0100

    Adjust build_steam-linux.sh to prime Steampipe

--- build_steam-linux.sh
+++ build_steam-linux.sh
@@ -28,16 +28,82 @@ if [ $? -ne 0 ]; then
     exit
 fi
 
-cp '/usr/lib/x86_64-linux-gnu/libSDL2-2.0.so.0' .
-cp '/usr/lib/x86_64-linux-gnu/libSDL2_image-2.0.so.0' .
-cp '/usr/lib/x86_64-linux-gnu/libSDL2_mixer-2.0.so.0' .
-cp '/usr/lib/x86_64-linux-gnu/libSDL2_ttf-2.0.so.0' .
-cp 'steamworks_sdk/sdk/redistributable_bin/linux64/libsteam_api.so' .
+#cp '/usr/lib/x86_64-linux-gnu/libSDL2-2.0.so.0' .
+#cp '/usr/lib/x86_64-linux-gnu/libSDL2_image-2.0.so.0' .
+#cp '/usr/lib/x86_64-linux-gnu/libSDL2_mixer-2.0.so.0' .
+#cp '/usr/lib/x86_64-linux-gnu/libSDL2_ttf-2.0.so.0' .
+#cp 'steamworks_sdk/sdk/redistributable_bin/linux64/libsteam_api.so' .
+#
+#zipname="exodus_steam-linux.zip"
+#name="$(basename "$(pwd)")"
+#
+#pushd ..
+#zip -r "${zipname}" "${name}" --include "${name}/assets/*" "${name}/exodus" "${name}/LICENSE" "${name}/manual.pdf" "${name}/README.md" "${name}/supplement.pdf" "${name}/libSDL2-2.0.so.0" "${name}/libSDL2_image-2.0.so.0" "${name}/libSDL2_mixer-2.0.so.0" "${name}/libSDL2_ttf-2.0.so.0" "${name}/libsteam_api.so" "${name}/licenses/*" "${name}/steam_appid.txt"
+#mv "${zipname}" "${name}"
+#popd
 
-zipname="exodus_steam-linux.zip"
-name="$(basename "$(pwd)")"
+target_dir="steamworks_sdk/sdk/tools/ContentBuilder/content/linux"
 
-pushd ..
-zip -r "${zipname}" "${name}" --include "${name}/assets/*" "${name}/exodus" "${name}/LICENSE" "${name}/manual.pdf" "${name}/README.md" "${name}/supplement.pdf" "${name}/libSDL2-2.0.so.0" "${name}/libSDL2_image-2.0.so.0" "${name}/libSDL2_mixer-2.0.so.0" "${name}/libSDL2_ttf-2.0.so.0" "${name}/libsteam_api.so" "${name}/licenses/*" "${name}/steam_appid.txt"
-mv "${zipname}" "${name}"
-popd
+rm -rf "$target_dir"
+mkdir -p "$target_dir"
+
+cp "exodus" "$target_dir"
+cp "steam_appid.txt" "$target_dir"
+cp -r "assets" "$target_dir"
+cp '/usr/lib/x86_64-linux-gnu/libSDL2-2.0.so.0' "$target_dir"
+cp '/usr/lib/x86_64-linux-gnu/libSDL2_image-2.0.so.0' "$target_dir"
+cp '/usr/lib/x86_64-linux-gnu/libSDL2_mixer-2.0.so.0' "$target_dir"
+cp '/usr/lib/x86_64-linux-gnu/libSDL2_ttf-2.0.so.0' "$target_dir"
+cp 'steamworks_sdk/sdk/redistributable_bin/linux64/libsteam_api.so' "$target_dir"
+cp "manual.pdf" "$target_dir"
+cp "supplement.pdf" "$target_dir"
+cp "README.md" "$target_dir"
+cp "LICENSE" "$target_dir"
+cp -r "licenses" "$target_dir"
+
+exodus_build='''
+"AppBuild"
+{
+    "AppID" "4098020"
+    "Desc" "Linux"
+    "Preview" "0"
+    "ContentRoot" "../content/"
+    "verbose" "1"
+    "Depots"
+    {
+        "4098021" "exodus_depot_build_4098021.vdf"
+    }
+}
+'''
+
+depot_build='''
+"DepotBuild"
+{
+    "DepotID" "4098021"
+
+    "FileMapping"
+    {
+        "LocalPath" "./linux/*"
+        "DepotPath" "."
+        "Recursive" "1"
+    }
+}
+'''
+
+echo "$exodus_build" > "steamworks_sdk/sdk/tools/ContentBuilder/scripts/exodus_build_4098020.vdf"
+echo "$depot_build" > "steamworks_sdk/sdk/tools/ContentBuilder/scripts/exodus_depot_build_4098021.vdf"
+
+echo ""
+echo "Next steps:"
+echo "  * Verify everything is present and correct under $target_dir"
+echo "  * cd steamworks_sdk/sdk/tools/ContentBuilder/builder_linux"
+echo "  * chmod +x steamcmd.sh"
+echo "  * chmod +x linux32/steamcmd"
+echo "  * ./steamcmd"
+echo "  * At the Steam prompt:"
+echo "    * login <YOUR STEAM USERNAME>"
+echo "    * Enter your password"
+echo "    * run_app_build ../scripts/exodus_build_4098020.vdf"
+echo "  * Go to the Steamworks web frontend"
+echo "  * Go to Steampipe > Builds"
+echo "  * Set the build live on the desired branch"
