9ed3b973b48a5dde4443b94487240e05c45b8bd3
Tom Pickering <tom@alephspace.net>
2025-10-17 18:02:17 +0100

    Add build scripts for Steam

---
+++ build_steam-linux.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+git diff --exit-code >/dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+    echo "There are uncommitted changes; please resolve these before running"
+    exit
+fi
+
+echo "PREREQUISITE STEPS:"
+echo "Download the Steam SDK from https://partner.steamgames.com/downloads/list"
+echo "*** CURRENTLY EXPECTS v1.62 ***"
+echo "mkdir steamworks_sdk"
+echo "cd steamworks_sdk"
+echo "unzip ~/Downloads/steamworks_sdk_xxx.zip"
+echo
+
+if [[ ! -d "steamworks_sdk" ]]; then
+    echo "Error: steamworks_sdk directory not found"
+    exit
+fi
+
+make clean
+make -j7 steam-linux
+
+if [ $? -ne 0 ]; then
+    echo "Build failed"
+    exit
+fi
+
+cp '/usr/lib/x86_64-linux-gnu/libSDL2-2.0.so.0' .
+cp '/usr/lib/x86_64-linux-gnu/libSDL2_image-2.0.so.0' .
+cp '/usr/lib/x86_64-linux-gnu/libSDL2_mixer-2.0.so.0' .
+cp '/usr/lib/x86_64-linux-gnu/libSDL2_ttf-2.0.so.0' .
+cp 'steamworks_sdk/sdk/redistributable_bin/linux64/libsteam_api.so' .
+
+zipname="exodus_steam-linux.zip"
+name="$(basename "$(pwd)")"
+
+pushd ..
+zip -r "${zipname}" "${name}" --include "${name}/assets/*" "${name}/exodus" "${name}/LICENSE" "${name}/manual.pdf" "${name}/README.md" "${name}/supplement.pdf" "${name}/*.so.0" "${name}/libsteam_api.so" "licenses/*"
+mv "${zipname}" "${name}"
+popd
--- build_steam-windows.sh
+++ build_steam-windows.sh
@@ -7,6 +7,19 @@ if [ $? -ne 0 ]; then
     exit
 fi
 
+echo "PREREQUISITE STEPS:"
+echo "Download the Steam SDK from https://partner.steamgames.com/downloads/list"
+echo "*** CURRENTLY EXPECTS v1.62 ***"
+echo "mkdir steamworks_sdk"
+echo "cd steamworks_sdk"
+echo "unzip ~/Downloads/steamworks_sdk_xxx.zip"
+echo
+
+if [[ ! -d "steamworks_sdk" ]]; then
+    echo "Error: steamworks_sdk directory not found"
+    exit
+fi
+
 mkdir -p include/x86_64-linux-gnu
 ln -sf /usr/include/SDL2 include
 ln -sf /usr/include/x86_64-linux-gnu/SDL2 include/x86_64-linux-gnu
@@ -28,7 +41,7 @@ cp SDL2_ttf-2.20.2/x86_64-w64-mingw32/lib/libSDL2_ttf.dll.a .
 popd
 
 make clean
-make -j7 windows
+make -j7 steam-windows
 
 if [ $? -ne 0 ]; then
     echo "Build failed"
@@ -39,6 +52,7 @@ cp lib/SDL2-2.26.5/x86_64-w64-mingw32/bin/SDL2.dll .
 cp lib/SDL2_image-2.6.3/x86_64-w64-mingw32/bin/SDL2_image.dll .
 cp lib/SDL2_mixer-2.6.3/x86_64-w64-mingw32/bin/SDL2_mixer.dll .
 cp lib/SDL2_ttf-2.20.2/x86_64-w64-mingw32/bin/SDL2_ttf.dll .
+cp steamworks_sdk/sdk/redistributable_bin/win64/steam_api64.dll .
 
 zipname="exodus_windows.zip"
 name="$(basename "$(pwd)")"
