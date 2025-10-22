9014c1bff46b935c23740194a88e1eb7b14c2e69
Tom Pickering <tom@alephspace.net>
2025-10-22 20:28:54 +0100

    Move PlanetReport implementation into cpp file

--- src/state/planet_report.cpp
+++ src/state/planet_report.cpp
@@ -1,1 +1,84 @@
 #include "planet_report.h"
+
+PlanetReport::PlanetReport() {
+    reset();
+}
+
+PlanetReport& PlanetReport::operator=(const PlanetReport& other) {
+    reset();
+
+    star_idx = other.star_idx;
+    planet_idx = other.planet_idx;
+    player_idx = other.player_idx;
+
+    items = other.items;
+    for (int i = 0; i < other.items; ++i) {
+        snprintf(content[i], REPORT_LINE_MAX, other.content[i]);
+    }
+
+    event_mask = other.event_mask;
+
+    return *this;
+}
+
+void PlanetReport::init(int star, int planet, int player) {
+    reset();
+    star_idx = star;
+    planet_idx = planet;
+    player_idx = player;
+}
+
+void PlanetReport::add_event(PlanetReportEvent event) {
+    event_mask = (event_mask | (1 << (int)event));
+}
+
+bool PlanetReport::has_event(PlanetReportEvent event) const {
+    return (event_mask & (1 << (int)event));
+}
+
+void PlanetReport::add_line(const char* msg, ...) {
+    if (items >= REPORT_LINES)
+        return;
+
+    content[items][REPORT_LINE_MAX - 1] = '\0';
+    va_list args;
+    va_start(args, msg);
+    vsnprintf(content[items], REPORT_LINE_MAX - 2, msg, args);
+    va_end(args);
+    items++;
+}
+
+bool PlanetReport::empty() const {
+    return (items == 0) && (event_mask == 0);
+}
+
+void PlanetReport::reset() {
+    star_idx = 0;
+    planet_idx = 0;
+    player_idx = 0;
+
+    for (int i = 0; i < REPORT_LINES; ++i) {
+        content[i][0] = '\0';
+    }
+
+    items = 0;
+    event_mask = 0;
+}
+
+void PlanetReport::save(cJSON* j) const {
+    SAVE_NUM(j, star_idx);
+    SAVE_NUM(j, planet_idx);
+    SAVE_NUM(j, player_idx);
+    SAVE_NUM(j, items);
+    SAVE_ARRAY_OF_STR(j, content);
+    SAVE_NUM(j, event_mask);
+}
+
+void PlanetReport::load(cJSON* j) {
+    LOAD_NUM(j, star_idx);
+    LOAD_NUM(j, planet_idx);
+    LOAD_NUM(j, player_idx);
+    LOAD_NUM(j, items);
+    LOAD_ARRAY_OF_STR(j, content);
+    LOAD_NUM(j, event_mask);
+}
--- src/state/planet_report.h
+++ src/state/planet_report.h
@@ -32,16 +32,10 @@ enum PlanetReportEvent {
 };
 
 struct PlanetReport : public Saveable {
-    PlanetReport() {
-        reset();
-    }
+    PlanetReport();
+    PlanetReport& operator=(const PlanetReport& other);
 
-    void init(int star, int planet, int player) {
-        reset();
-        star_idx = star;
-        planet_idx = planet;
-        player_idx = player;
-    }
+    void init(int star, int planet, int player);
 
     int star_idx;
     int planet_idx;
@@ -52,78 +46,14 @@ struct PlanetReport : public Saveable {
 
     uint32_t event_mask;
 
-    void add_event(PlanetReportEvent event) {
-        event_mask = (event_mask | (1 << (int)event));
-    }
+    void add_event(PlanetReportEvent event);
+    bool has_event(PlanetReportEvent event) const;
+    void add_line(const char* msg, ...);
+    bool empty() const;
+    void reset();
 
-    bool has_event(PlanetReportEvent event) const {
-        return (event_mask & (1 << (int)event));
-    }
-
-    void add_line(const char* msg, ...) {
-        if (items >= REPORT_LINES)
-            return;
-
-        content[items][REPORT_LINE_MAX - 1] = '\0';
-        va_list args;
-        va_start(args, msg);
-        vsnprintf(content[items], REPORT_LINE_MAX - 2, msg, args);
-        va_end(args);
-        items++;
-    }
-
-    bool empty() const {
-        return (items == 0) && (event_mask == 0);
-    }
-
-    void reset() {
-        star_idx = 0;
-        planet_idx = 0;
-        player_idx = 0;
-
-        for (int i = 0; i < REPORT_LINES; ++i) {
-            content[i][0] = '\0';
-        }
-
-        items = 0;
-
-        event_mask = 0;
-    }
-
-    PlanetReport& operator=(const PlanetReport& other) {
-        reset();
-
-        star_idx = other.star_idx;
-        planet_idx = other.planet_idx;
-        player_idx = other.player_idx;
-
-        items = other.items;
-        for (int i = 0; i < other.items; ++i) {
-            snprintf(content[i], REPORT_LINE_MAX, other.content[i]);
-        }
-
-        event_mask = other.event_mask;
-
-        return *this;
-    }
-
-    virtual void save(cJSON* j) const override {
-        SAVE_NUM(j, star_idx);
-        SAVE_NUM(j, planet_idx);
-        SAVE_NUM(j, player_idx);
-        SAVE_NUM(j, items);
-        SAVE_ARRAY_OF_STR(j, content);
-        SAVE_NUM(j, event_mask);
-    }
-
-    virtual void load(cJSON* j) override {
-        LOAD_NUM(j, star_idx);
-        LOAD_NUM(j, planet_idx);
-        LOAD_NUM(j, player_idx);
-        LOAD_NUM(j, items);
-        LOAD_ARRAY_OF_STR(j, content);
-        LOAD_NUM(j, event_mask);
-    }
+    virtual void save(cJSON* j) const override;
+    virtual void load(cJSON* j) override;
 };
 
 #endif
