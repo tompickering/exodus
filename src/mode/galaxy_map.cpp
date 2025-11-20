a367f51e31122b696fae3deaedbe0ec711e86921
Tom Pickering <tom@alephspace.net>
2025-11-20 00:51:19 +0000

    Reset planet report index on month pass start

--- src/mode/galaxy_map.cpp
+++ src/mode/galaxy_map.cpp
@@ -518,6 +518,7 @@ ExodusMode GalaxyMap::update(float delta) {
         case GM_MonthPassStart:
             // Autosave
             save_manager.save(0);
+            ephstate.planet_report_idx = -1;
             set_stage(GM_MonthPassing);
             return ExodusMode::MODE_None;
         case GM_MonthPassing:
