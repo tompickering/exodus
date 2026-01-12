50840b404b5eb2036173a7adeb3c4b40d085e4b8
Tom Pickering <tom@alephspace.net>
2026-01-12 17:12:12 +0000

    Jan's changes to support extended ASCII

--- src/util/str.cpp
+++ src/util/str.cpp
@@ -8,6 +8,11 @@
 void capitalise(char* str, int len) {
     for (int i = 0; i < len; ++i) {
         str[i] = toupper(str[i]);
+
+        // JK: Also uppercase the extended ASCII characters:
+        if ((str[i] < -3) && (str[i] > -33)){
+            str[i] -=32;
+        }
     }
 }
 
@@ -18,6 +23,11 @@ const char* tmp_caps(const char* str) {
     for (i = 0; i < strnlen(str, CAP_MAX_LEN - 1); ++i) {
         if (str[i] == '\0') break;
         _tmp_caps[i] = toupper(str[i]);
+
+        // JK: Also uppercase the extended ASCII characters:
+        if ((_tmp_caps[i] < -3) && (_tmp_caps[i] > -33)){
+            _tmp_caps[i] -=32;
+        }
     }
     _tmp_caps[i] = '\0';
     return (const char*)(&_tmp_caps);
